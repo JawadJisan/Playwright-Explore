bd5e9cf48cafb812798e26aa2df6bb0f1da27e46
zahidmiran <zahidmiron599@gmail.com>
2024-02-15 19:03:07 +0600

    write test case with selector & locator

--- tests/selector.spec.js
+++ tests/selector.spec.js
@@ -5,4 +5,13 @@ test("Selectors Test Demo", async ({ page }) => {
   await page.pause();
   await page.click("id=user-name");
   await page.locator("id=user-name").fill("Jawad Jisan");
+  //using XPath
+  await page.locator('xpath=//input[@name="secret_sauce"]').fill("1234567890");
+
+  //   using CSS Selector
+  await page.locator("#login-button").click();
+  //   using TEXT
+  await page.locator('text="LOGIN"').click();
+  //   await page.locator(':has-text("LOGIN)').click();
+  await page.locator('input:has-text("LOGIN)').click();
 });
